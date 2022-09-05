422b1a6ea0697ef0de89a31a4350d22cdf8ef271
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-05 16:28:30 +0200

    Add ADP toolchain configuration template

---
+++ jenkins/job-description/ERERERERER22_view.groovy
@@ -0,0 +1,16 @@
+ 
+listView('ERERERERER22 Jobs') {
+    description('ERERERERER22 Jobs')
+    jobs {
+        regex('ERERERERER22_.+')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
